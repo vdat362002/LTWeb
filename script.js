d71e0d458d10ffcc6c20ff23887b74fa18e538b1
Nguyễn Hoàng Hải <ln26805@gmail.com>
2022-11-01 13:58:06 +0700

    upload

--- script.js
+++ script.js
@@ -3,21 +3,25 @@ const leftbtn = document.querySelector('.fa-chevron-left')
 const imgNb = document.querySelectorAll('.slider-content-left-top img')
 const imgNbLi = document.querySelectorAll('.slider-content-left-bottom li')
 console.log(imgNb)
-let index = 0;
+let temp = 0;
 rightbtn.addEventListener('click', function() {
-    index = index + 1
-    if (index > imgNb.length-1) {
-        index = 0
+    removeactive()
+    temp = temp + 1
+    if (temp > imgNb.length-1) {
+        temp = 0
     }
-    document.querySelector(".slider-content-left-top").style.right = index  *100 + "%"
+    imgNbLi[temp].classList.add("active")
+    document.querySelector(".slider-content-left-top").style.right = temp  *100 + "%"
 })
 
 leftbtn.addEventListener('click', function() {
-    index = index - 1
-    if (index < 0) {
-        index = imgNb.length - 1 
+    removeactive()
+    temp = temp - 1
+    if (temp < 0) {
+        temp = imgNb.length - 1 
     }
-    document.querySelector(".slider-content-left-top").style.right = index  *100 + "%"
+    imgNbLi[temp].classList.add("active")
+    document.querySelector(".slider-content-left-top").style.right = temp  *100 + "%"
 })
 
 imgNbLi.forEach(function(image,index) {
@@ -25,6 +29,7 @@ imgNbLi.forEach(function(image,index) {
         removeactive()
         document.querySelector(".slider-content-left-top").style.right = index  *100 + "%"
         image.classList.add("active")
+        temp = index
     })
 })
 
@@ -34,13 +39,19 @@ function removeactive () {
 }
 
 function imgAuto () {
-    index = index + 1
-    if (index > imgNb.length-1) {
-        index = 0
+    temp = temp + 1
+    if (temp > imgNb.length-1) {
+        temp = 0
     }
     removeactive()
-    document.querySelector(".slider-content-left-top").style.right = index  *100 + "%"
-    imgNbLi[index].classList.add("active")
+    document.querySelector(".slider-content-left-top").style.right = temp  *100 + "%"
+    imgNbLi[temp].classList.add("active")
+    if(imgNbLi.forEach(function(image,index) {
+        image.addEventListener('click', function(){
+        })
+    })){
+        clearInterval(imgAuto)
+    }
 }
 
 setInterval(imgAuto,3000)
